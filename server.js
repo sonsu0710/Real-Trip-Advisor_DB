d53e77d0289d169c198ff8fa9f0fdf411f9c5c69
sonsu0710 <sonsu0710@gmail.com>
2022-12-28 00:27:53 +0900

    make it better

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 5000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log('JSON Server is running');
+});
